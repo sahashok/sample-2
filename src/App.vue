a5d62e390d783e7c109b71368240a82bdb64015a
Ashok Sah <sahashok498@gmail.com>
2023-12-28 11:26:47 +0545

    start commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Welcome To vue js</h1>
+    <SignUp />
+    <HomeCom />
+    <NewOrbit />
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+</template>
+
+<script>
+import HomeCom from './components/HomeCom.vue';
+import NewOrbit from './components/NewOrbit.vue';
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'App',
+  components:{
+    SignUp,
+    HomeCom,
+    NewOrbit,
+}
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewOrbit.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="New">
+        <h1>New Orbit Computer Institute</h1>
+
+<div>
+    <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px"></v-main>
+    </v-layout>
+  </v-card>
+
+</div>
+
+
+    </div>
+</template>
+<script>
+export default {
+    name:"NewOrbit",
+}
+</script>
+
+<style>
+.New{
+    /* background-color: yellowgreen; */
+}
+</style>
